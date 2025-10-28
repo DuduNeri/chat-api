da211fce7f3100cd8c524a664a4c952f6ba31a2a
DuduNeri <nerimartinseduardo@gmail.com>
2025-10-28 19:00:49 -0300

    Adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors"
 import userRoutes from "./routes/user.routes";
 import conversationRouter from "./routes/conversation.routes";
 import messageRouter from "./routes/message.routes";
 import { loginRouter } from "./routes/login.route";
 
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes); 
